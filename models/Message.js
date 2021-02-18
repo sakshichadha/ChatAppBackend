63e8bbd3131b3e5e033846e961dae73926f2b681
Aashish <singh.aashish2399@gmail.com>
2021-02-18 13:48:02 +0530

    Add message schema

---
+++ models/Message.js
@@ -0,0 +1,33 @@
+const { Schema } = require("mongoose");
+
+const MessageSchema = new Schema({
+    body: {
+        type: String, 
+        required: true,
+    },
+    media: {
+        type: String,
+    },
+    date:{
+        type: Date,
+        default: Date.now(),
+    },
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true,
+    },
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+
+
+});
+
+module.exports = mongoose.model('message',MessageSchema);
